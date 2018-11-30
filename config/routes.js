763947ec613eba5f7a77c9c3d9013e652e6f56b0
DaftBeowulf <ajmboosh@gmail.com>
2018-11-30 10:41:03 -0600

    register endpoint working as intended

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,33 @@
-const axios = require('axios');
-
-const { authenticate } = require('./middlewares');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { authenticate } = require("./middlewares");
+const db = require("../database/dbConfig");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+
+  if (creds.username && creds.password) {
+    const hash = bcrypt.hashSync(creds.password, 12);
+    creds.password = hash;
+    db("users")
+      .insert(creds)
+      .then(id => {
+        res.status(201).json(id);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Error while registering user: ", err });
+      });
+  } else {
+    res.status(400).json({ message: "Please fill all fields." });
+  }
 }
 
 function login(req, res) {
@@ -19,12 +37,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
